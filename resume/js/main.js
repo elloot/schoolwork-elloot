2b9ec1fb4093e4a166ed3bf5a9b495295cb97ca6
elloot <elliot.duchek@gmail.com>
2020-03-10 09:11:56 +0100

    add webp feature check

--- resume/js/main.js
+++ resume/js/main.js
@@ -78,3 +78,36 @@ let desktopQuery = window.matchMedia("(min-width: 992px)");
         });
     });
 })();
+
+(() => {
+    // check_webp_feature:
+    //   'feature' can be one of 'lossy', 'lossless', 'alpha' or 'animation'.
+    //   'callback(feature, result)' will be passed back the detection result (in an asynchronous way!)
+
+    check_webp_feature(0, myFunction);
+
+    function check_webp_feature(feature, callback) {
+        var kTestImages = {
+            lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+            lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+            alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+            animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
+        };
+        var img = new Image();
+        img.onload = function() {
+            var result = img.width > 0 && img.height > 0;
+            callback(feature, result);
+        };
+        img.onerror = function() {
+            callback(feature, false);
+        };
+        img.src = "data:image/webp;base64," + kTestImages[feature];
+    }
+
+    function myFunction(feature, result) {
+        console.log(result);
+        if (!result) {
+            document.querySelector(".card__portrait").setAttribute("src", "img/portrait.jpg");
+        }
+    }
+})();
