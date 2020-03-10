742d821180edd5299902f04451f6206e09106694
elloot <elliot.duchek@gmail.com>
2020-03-10 09:51:44 +0100

    add js to check if browser supports webp

--- resume/js/main.js
+++ resume/js/main.js
@@ -79,35 +79,22 @@ let desktopQuery = window.matchMedia("(min-width: 992px)");
     });
 })();
 
-(() => {
-    // check_webp_feature:
-    //   'feature' can be one of 'lossy', 'lossless', 'alpha' or 'animation'.
-    //   'callback(feature, result)' will be passed back the detection result (in an asynchronous way!)
+async function supportsWebp() {
+    if (!self.createImageBitmap) return false;
 
-    check_webp_feature(0, myFunction);
+    const webpData = "data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=";
+    const blob = await fetch(webpData).then(r => r.blob());
+    return createImageBitmap(blob).then(
+        () => true,
+        () => false
+    );
+}
 
-    function check_webp_feature(feature, callback) {
-        var kTestImages = {
-            lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
-            lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
-            alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
-            animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
-        };
-        var img = new Image();
-        img.onload = function() {
-            var result = img.width > 0 && img.height > 0;
-            callback(feature, result);
-        };
-        img.onerror = function() {
-            callback(feature, false);
-        };
-        img.src = "data:image/webp;base64," + kTestImages[feature];
-    }
-
-    function myFunction(feature, result) {
-        /*console.log(result);
-        if (!result) {
-            document.querySelector(".card__portrait").setAttribute("src", "img/portrait.jpg");
-        }*/
+(async () => {
+    if (await supportsWebp()) {
+        console.log("does support webp");
+        document.querySelector(".card__portrait").setAttribute("src", "./img/portrait.webp");
+    } else {
+        console.log("does not support webp");
     }
 })();
